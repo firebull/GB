67374171f2fdc3ed8c3612add5dbc20b819a46d1
Nikita Bulaev <djfirebull@gmail.com>
2019-04-25 16:00:57 +0300

    Закончил ДЗ к уроку №3

--- OOP/Lesson 3/task3.cpp
+++ OOP/Lesson 3/task3.cpp
@@ -25,11 +25,14 @@ class Fraction {
     bool     isCorrect;      /*!< Корректная ли дробь */
     int      sign;           /*!< Знак дроби (+1 или -1) */
     unsigned intPart;        /*!< Целая часть дроби */
-    unsigned numerator;      /*!< Числитель дроби */
+    long     numerator;      /*!< Числитель дроби */
     unsigned denominator;    /*!< Знаменатель дроби */
     double   resFraction;    /*!< Итоговое число с плавающей точкой */
+
+    virtual void GetIntPart()    = 0;
+
   public:
-    virtual void GetIntPart() = 0;
+    virtual void printFraction() = 0;
 
     /** Операция преобразования дроби в тип double */
     operator double() {
@@ -44,6 +47,9 @@ class Fraction {
 
 
 class SimpleFraction : public Fraction {
+  protected:
+    /** Выделение целой части дроби, заглушка функции */
+    void GetIntPart() {};
   public:
     SimpleFraction() {
         isCorrect = false;
@@ -67,17 +73,24 @@ class SimpleFraction : public Fraction {
         intPart     = 0;
         numerator   = n;
         denominator = d;
-
-        GetIntPart();
     }
 
-    void GetIntPart() {};
+    /** Вывод дроби в консоль */
+    void printFraction() {
+        if (sign < 0) {
+            std::cout << "-";
+        }
+
+        std::cout << numerator << "/" << denominator << std::endl;
+    }
 
     /** Сложение двух дробей */
     SimpleFraction operator + (SimpleFraction a) {
         SimpleFraction res;
-        res.numerator = sign * (denominator + numerator) * a.denominator +
-                        a.sign * (a.denominator + a.numerator) * denominator;
+
+        res.numerator = sign * numerator * a.denominator +
+                        a.sign * a.numerator * denominator;
+
         res.denominator = denominator * a.denominator;
 
         res.intPart     = 0;
@@ -86,18 +99,87 @@ class SimpleFraction : public Fraction {
         return res;
     }
 
-    bool operator < (SimpleFraction b) {
-        if (double(*this) >= double(b)) { return false; }
+    /** Вычитание двух дробей */
+    SimpleFraction operator - (SimpleFraction a) {
+        SimpleFraction res(1, 0, 1);
 
-        return true;
+        res.numerator = sign * numerator * a.denominator -
+                        a.sign * a.numerator * denominator;
+
+        res.denominator = denominator * a.denominator;
+
+        if (res.numerator < 0) {
+            res.sign = -1;
+            res.numerator *= -1;
+        }
+
+        return res;
     }
 
+    /** Умножение двух дробей */
+    SimpleFraction operator * (SimpleFraction a) {
+        SimpleFraction res(1, 0, 1);
+
+        res.sign        = sign * a.sign;
+        res.numerator   = numerator * a.numerator;
+        res.denominator = denominator * a.denominator;
+
+        return res;
+    }
+
+    /** Деление двух дробей */
+    SimpleFraction operator / (SimpleFraction a) {
+        SimpleFraction res(1, 0, 1);
+
+        res.sign        = sign * a.sign;
+        res.numerator   = numerator * a.denominator;
+        res.denominator = denominator * a.numerator;
+
+        return res;
+    }
+
+    /** Логическая операция сравнения двух дробей "меньше чем" */
+    bool operator < (SimpleFraction b) {
+        return double(*this) < double(b);
+    }
+
+    /** Логическая операция сравнения двух дробей "больше чем" */
+    bool operator > (SimpleFraction b) {
+        return double(*this) > double(b);
+    }
+
+    /** Логическая операция сравнения двух дробей "больше или равно чем" */
     bool operator >= (SimpleFraction b) {
-        return !(double(*this) < double(b));
+        return !(*this < b);
+    }
+
+    /** Логическая операция сравнения двух дробей "меньше или равно чем" */
+    bool operator <= (SimpleFraction b) {
+        return !(*this > b);
+    }
+
+    /** Логическая операция сравнения двух дробей */
+    bool operator == (SimpleFraction b) {
+        return double(*this) == double(b);
+    }
+
+    /** Унарная операция изменения знака дроби */
+    SimpleFraction operator - () {
+        SimpleFraction res(sign * -1, numerator, denominator);
+        return res;
     }
 };
 
 class MixedFraction : public Fraction {
+  protected:
+    /** Выделение целой части дроби */
+    void GetIntPart() {
+        if (numerator >= denominator) {
+            intPart += (numerator / denominator);
+            numerator %= denominator;
+        }
+    }
+
   public:
     MixedFraction() {
         isCorrect = false;
@@ -124,11 +206,98 @@ class MixedFraction : public Fraction {
         GetIntPart();
     }
 
-    void GetIntPart() {
-        if (numerator >= denominator) {
-            intPart += (numerator / denominator);
-            numerator %= denominator;
+    /** Вывод дроби в консоль */
+    void printFraction() {
+        if (sign < 0) {
+            std::cout << "-";
         }
+
+        std::cout << intPart << " " << numerator << "/" << denominator << std::endl;
+    }
+
+    /** Сложение двух дробей */
+    MixedFraction operator + (MixedFraction a) {
+        MixedFraction res(1, 0, 0, 1);
+
+        res.numerator = sign * (intPart * denominator + numerator) * a.denominator +
+                        a.sign * (a.intPart * a.denominator + a.numerator) * denominator;
+        res.denominator = denominator * a.denominator;
+
+        res.GetIntPart();
+
+        return res;
+    }
+
+    /** Вычитание двух дробей */
+    MixedFraction operator - (MixedFraction a) {
+        MixedFraction res(1, 0, 0, 1);
+
+        res.numerator = sign * (intPart * denominator + numerator) * a.denominator -
+                        a.sign * (a.intPart * a.denominator + a.numerator) * denominator;
+        res.denominator = denominator * a.denominator;
+
+        if (res.numerator < 0) {
+            res.sign = -1;
+            res.numerator *= -1;
+        }
+
+        res.GetIntPart();
+        return res;
+    }
+
+    /** Умножение двух дробей */
+    MixedFraction operator * (MixedFraction a) {
+        MixedFraction res(1, 0, 0, 1);
+
+        res.sign        = sign * a.sign;
+        res.numerator   = (intPart * denominator + numerator) * (a.intPart * a.denominator + a.numerator);
+        res.denominator = denominator * a.denominator;
+
+        res.GetIntPart();
+        return res;
+    }
+
+    /** Деление двух дробей */
+    MixedFraction operator / (MixedFraction a) {
+        MixedFraction res(1, 0, 0, 1);
+
+        res.sign        = sign * a.sign;
+        res.numerator   = (intPart * denominator + numerator) * a.denominator;
+        res.denominator = denominator * (a.intPart * a.denominator + a.numerator);
+
+        res.GetIntPart();
+        return res;
+    }
+
+    /** Логическая операция сравнения двух дробей "меньше чем" */
+    bool operator < (MixedFraction b) {
+        return double(*this) < double(b);
+    }
+
+    /** Логическая операция сравнения двух дробей "больше чем" */
+    bool operator > (MixedFraction b) {
+        return double(*this) > double(b);
+    }
+
+    /** Логическая операция сравнения двух дробей "больше или равно чем" */
+    bool operator >= (MixedFraction b) {
+        return !(*this < b);
+    }
+
+    /** Логическая операция сравнения двух дробей "меньше или равно чем" */
+    bool operator <= (MixedFraction b) {
+        return !(*this > b);
+    }
+
+    /** Логическая операция сравнения двух дробей */
+    bool operator == (MixedFraction b) {
+        return double(*this) == double(b);
+    }
+
+    /** Унарная операция изменения знака дроби */
+    MixedFraction operator - () {
+        MixedFraction res(sign * -1, intPart, numerator, denominator);
+        return res;
     }
 };
 
@@ -138,7 +307,24 @@ int main (int argc, char ** args) {
     SimpleFraction n1(1, 3, 4);
     SimpleFraction n2(1, 2, 4);
 
-    std::cout << (n1 >= n2) << std::endl;
+    std::cout << "Logic n1 <= n2 is: " << (n1 <= n2) << std::endl;
+
+    SimpleFraction sn = n1 / n2;
+
+    sn.printFraction();
+
+    MixedFraction n3(1, 1, 1, 4);
+    MixedFraction n4(1, 1, 2, 5);
+
+    std::cout << "Logic n3 >= n4 is: " << (n3 >= n4) << std::endl;
+
+    MixedFraction mn = n3 / n4;
+
+    mn.printFraction();
+
+    MixedFraction mm = -n3;
+
+    mm.printFraction();
 
     return 0;
 }
