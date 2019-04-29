66ee27a59f7236da9f5f7f4819d6dbcd0da275f6
Nikita Bulaev <djfirebull@gmail.com>
2019-04-29 12:35:50 +0300

    ДЗ1 урока №4

--- OOP/Lesson 4/task1.cpp
+++ OOP/Lesson 4/task1.cpp
@@ -25,6 +25,40 @@ class ArrayInt {
     int m_length;
     int * m_data;
 
+    /**
+    * @brief Быстрая сортировка через рекурсию
+    *
+    * @param first Номер стартового элемента
+    * @param last  Номер конечного элемента
+    */
+    void sortArray(int first, int last) {
+
+        if (first < last) {
+            int left   = first,
+                right  = last,
+                medium = m_data[(left + right) / 2];
+
+            do {
+                while (m_data[left] < medium) {
+                    left++;
+                };
+
+                while (m_data[right] > medium) {
+                    right--;
+                };
+
+                if (left <= right) {
+                    SWAP(m_data[left], m_data[right]);
+                    left++;
+                    right--;
+                }
+            } while (left <= right);
+
+            sortArray(first, right);
+            sortArray(left, last);
+        }
+    }
+
   public:
     ArrayInt(): m_length(0), m_data(nullptr) {};
 
@@ -42,6 +76,7 @@ class ArrayInt {
         delete[] m_data;
     }
 
+    /** Очистка массива */
     void erase() {
         delete[] m_data;
 
@@ -49,6 +84,11 @@ class ArrayInt {
         m_length = 0;
     }
 
+    /**
+     * @brief Возвращает текущую длину массива
+     *
+     * @return Длина массива
+     */
     int getLength() {
         return m_length;
     }
@@ -96,6 +136,12 @@ class ArrayInt {
         m_length = newLength;
     }
 
+    /**
+     * @brief Вставляет элемент в массив
+     *
+     * @param value Что вставить
+     * @param index Куда вставить
+     */
     void insertBefore(int value, int index) {
         // Проверка корректности передаваемого индекса
         assert(index >= 0 && index <= m_length);
@@ -122,64 +168,139 @@ class ArrayInt {
         ++m_length;
     }
 
-    void push_back(int value) {
+    ArrayInt & push_back(int value) {
         insertBefore(value, m_length);
-    }
 
-    void del_element(int index) {
-
-    }
-
-
-    void pop_back() {
-        del_element(m_length - 1);
+        return *this;
     }
 
     /**
-    * @brief Быстрая сортировка
-    *
-    * @param arr   Массив данных
-    * @param first Номер стартового элемента
-    * @param last  Номер конечного элемента
-    */
-    void sort(int first, int last) {
+     * @brief Удаляет элемент массива
+     *
+     * @param index Индекс элемента для удаления
+     */
+    void del_element(int index) {
+        // Проверка корректности передаваемого индекса
+        assert(m_length > 0 && index >= 0 && index <= m_length);
 
-        if (first < last) {
-            int left   = first,
-                right  = last,
-                medium = m_data[(left + right) / 2];
-
-            do {
-                while (m_data[left] < medium) {
-                    left++;
-                };
-
-                while (m_data[right] > medium) {
-                    right--;
-                };
-
-                if (left <= right) {
-                    SWAP(m_data[left], m_data[right]);
-                    left++;
-                    right--;
-                }
-            } while (left <= right);
-
-            sort(first, right);
-            sort(left, last);
+        if (m_length == 1) {
+            erase();
         }
+
+        // Создаем новый массив на один элемент больше старого массива
+        int *data = new int[m_length - 1];
+
+        // Копируем все значения кроме удаляемого
+        for (int i = 0, j = 0; i < m_length; ++i) {
+            if (i == index) {
+                continue;
+            }
+
+            data[j++] = m_data[i];
+        }
+
+        // Удаляем старый массив и используем вместо него новый
+        delete[] m_data;
+        m_data = data;
+        --m_length;
     }
 
+    /** Удаляет последний элемент массива */
+    ArrayInt & pop_back() {
+        del_element(m_length - 1);
 
+        return *this;
+    }
+
+    /**
+     * @brief Вставляет другой массив в текущий
+     *
+     * @param newArr  Другой массив
+     * @param arrSize Размер другого массива
+     * @param index   Куда вставить
+     */
+    void insert_array(int newArr[], int arrSize, int index) {
+        assert(newArr != nullptr && index >= 0 && index <= m_length);
+
+        if (arrSize == 0) {
+            return;
+        }
+
+        int *data = new int[m_length + arrSize];
+
+        int i = 0,
+            n = 0;
+
+        for (; i <= index; ++i, ++n) {
+            data[i] = m_data[i];
+        }
+
+        for (int j = 0; i <= index + arrSize; ++i, ++j) {
+            data[i] = newArr[j];
+        }
+
+        for (; i < m_length + arrSize; ++n, ++i) {
+            data[i] = m_data[n];
+        }
+
+        // Удаляем старый массив и используем вместо него новый
+        delete[] m_data;
+        m_data = data;
+        m_length += arrSize;
+    }
+
+    /** Сортировка текущего массива */
+    ArrayInt & sort() {
+        sortArray(0, m_length - 1);
+
+        return *this;
+    }
+
+    /** Вывод текущего массива в консоль */
+    void print() {
+        std::cout << "[ ";
+
+        for (int i = 0; i < m_length; ++i) {
+            std::cout << m_data[i];
+
+            if (i < m_length - 1) {
+                std::cout << ", ";
+            }
+        }
+
+        std::cout << " ]" << std::endl;
+    }
+
+    /** Возвращаем элемент массива */
     int &operator[](int index) {
         assert(index >= 0 && index < m_length);
         return m_data[index];
     }
-
 };
 
-
 int main (int argc, char ** args) {
+
+    ArrayInt arr1;
+
+    arr1.push_back(1);
+    arr1.push_back(2);
+    arr1.push_back(3);
+    arr1.push_back(4);
+    arr1.push_back(5);
+
+    int *arr2 = new int[3];
+    arr2[0] = 10;
+    arr2[1] = 11;
+    arr2[2] = 12;
+
+    arr1.insert_array(arr2, 3, 2);
+
+    arr1.print();
+
+    arr1.sort().print();
+
+    arr1.del_element(0);
+    arr1.pop_back().print();
 
     return 0;
 }
